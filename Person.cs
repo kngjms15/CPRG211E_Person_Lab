64ce178b857225c441477b594cb5efee1f3e4b95
kngjms15 <118232516+kngjms15@users.noreply.github.com>
2023-01-25 18:07:11 -0700

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211E_Person_Lab
+{
+    internal class Person
+    {
+        //person instance variables to hold data
+        private int personId;
+        private string firstName;
+        private string lastName;
+        private string favoriteColour;
+        private int age;
+        private bool isWorking;
+
+        // classes have one or more constructors
+        public Person(int personId, string firstName, string lastName, string favoriteColour, int age, bool isWorking)
+        {
+            this.personId = personId;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.favoriteColour = favoriteColour;
+            this.age = age;
+            this.isWorking = isWorking;
+        }
+        //to Display person informations
+        public void DisplayPersonInfo()
+        {
+            string faveCol = $"{personId}: {firstName} {lastName}'s favorite colour is {favoriteColour}";
+            Console.WriteLine(faveCol);
+        }
+        // change favourite color
+        public void ChangeFavoriteColour()
+        {
+            favoriteColour = "White";
+            string faveCol = $"{personId}: {firstName} {lastName}'s favorite colour is {favoriteColour}";
+            Console.WriteLine(faveCol);
+        }
+        //to string method format
+        public override string ToString()
+        {
+            return ($"PersonID: {personId}\nFirstName: {firstName}\nLastName: {lastName}\nFavoriteColour: {favoriteColour}\nAge: {age}\nIsWorking: {isWorking}"); 
+        }
+        //to string method to add age
+        public void AddAge()
+        {
+            int added = age += 10;
+            string adding = $"{firstName} {lastName}'s age in 10 years is {added}";
+            Console.WriteLine(adding);
+        }
+    }
+
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211E_Person_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Person 1
+            Person person1;
+            person1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+
+            //Person2
+            Person person2;
+            person2 = new Person(2, "Gina", "James", "Green", 18, false);
+            
+            //Person3
+            Person person3;
+            person3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            
+            //Person4
+            Person person4;
+            person4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+
+            //Run program outputs
+            person2.DisplayPersonInfo();
+            Console.WriteLine(person3.ToString());
+            person1.ChangeFavoriteColour();
+            person4.AddAge();
+            Console.WriteLine(person3.ToString());
+            Console.WriteLine(person4.ToString());
+            //person1.DisplayPersonInfo();
+
+
+
+
+        }
+    }
+}
